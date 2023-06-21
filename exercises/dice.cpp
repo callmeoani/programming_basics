d4c33b0b6cdd04f81276bdaae4f36577cbc05705
Abdulhameed Muhammed <callmeoani@gmail.com>
2023-06-21 04:26:03 +0100

    tinkering with dice

--- exercises/dice.cpp
+++ exercises/dice.cpp
@@ -47,16 +47,26 @@ void full_die (int n) {
 
 }
 
+
+int row_die(){
+    srand(static_cast<unsigned>(time(0)));
+
+    return (rand() % 6) +1;
+}
 int main() {
 
     srand(static_cast<unsigned>(time(0)));
 
     int die_1= (rand() % 6) + 1;
     int die_2= (rand() % 6) + 1;
+    // int die_3 = row_die();
+    // int die_4 = row_die();
 
 
     full_die(die_1);
     full_die(die_2);
+    // full_die(die_3);
+    // full_die(die_4);
 
 
     // std::cout << die_1 << " " << die_2 << '\n';
